1fbfd131f4fe7847e73370985c03d88c5e9dcf49
Luis Reyes <luisreyesxv@yahoo.com>
2020-12-17 18:47:47 -0500

    I have the application sending out cookies. Also changed the Cors file to take a variable from credentials file. Future uses has to make sure that secret is stored in there.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,12 @@
 class API::V1::UsersController < ApplicationController
+    skip_before_action :authorized
+    
     def index
+        # cookies.signed[:jwt] = {value: "swagberto", httponly: true}
+        # cookies.delete(:jwt)
         users = User.all
         render json:users
     end
+
+   
 end
