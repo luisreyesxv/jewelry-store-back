69d851cc009045ee046412eb43a71f8276a73283
Luis Reyes <luisreyesxv@yahoo.com>
2020-12-20 23:47:02 -0500

    started to set up the user controller tests

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,12 +1,13 @@
-# require 'rails_helper'
-# require 'faker'
-# describe "get all questions route", :type => :request do
-#   let!(:questions) {FactoryBot.create_list(:random_question, 20)}
-# before {get '/api/v1/questions'}
-# it 'returns all questions' do
-#     expect(JSON.parse(response.body).size).to eq(20)
-#   end
-# it 'returns status code 200' do
-#     expect(response).to have_http_status(:success)
-#   end
-# end
+require 'rails_helper'
+require 'faker'
+describe "User routes", :type => :request do
+  
+before {get '/api/v1/users'}
+it 'returns all questions' do
+   puts User.count
+    expect(JSON.parse(response.body).size).to eq(20)
+  end
+it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+end
